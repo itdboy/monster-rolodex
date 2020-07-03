f62421fb43bb56a29a92450bcc35df68bf9b696d
itdboy <itdboy@gmail.com>
2020-07-03 11:04:39 +0700

    Updates

---
+++ precache-manifest.e560ed9a36bc9de1598beee59bb84569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b09670704ad092498b5eb0b8bdc0abb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "542db05fd2524790bae8",
+    "url": "/monsters-rolodex/static/css/main.44026783.chunk.css"
+  },
+  {
+    "revision": "efdaa8f6541fe00250cb",
+    "url": "/monsters-rolodex/static/js/2.dbc8e97e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.dbc8e97e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "542db05fd2524790bae8",
+    "url": "/monsters-rolodex/static/js/main.24dde996.chunk.js"
+  },
+  {
+    "revision": "e01b263a933f37eb7c2d",
+    "url": "/monsters-rolodex/static/js/runtime-main.6136c16c.js"
+  }
+]);
